cabd745c4bbe349f3aa30de89f8d34ff294e8feb
alimansi1382 <92879191+alimansi1382@users.noreply.github.com>
2022-06-26 21:38:56 +0430

    create cart page

--- public/templates/page/cart.php
+++ public/templates/page/cart.php
@@ -1,3 +1,79 @@
 <main class="content p1">
-    
+    <section class="grid-display col-sm-1 col-md-1 col-lg-3 ">
+                <section class="content-box bg-white grid-lg-1to3">
+                    <table class="table">
+                        <tr class="cartitem">
+                            <td class="cartitem_image"><img  src='img/p1.jpg' alt='' width='50px'/></td>
+                            <td class="cartitem_content">
+                                <h6 class="cartitem_content_name">موبایل</h6>
+                                <div class='cartitem_content_price'>
+                                    300,000
+                                    <span class="price_component">ریال</span>
+                                </div>
+                                <div class='cartitem_content_quantity'>
+                                    <div class="quantity">
+                                        <a href='' class='quantity_btn'>+</a>
+                                            <span class='quantity_value'>3</span>
+                                        <a href='' class='quantity_btn'>-</a>
+                                    </div>
+                                </div>
+                                <div class='cartitem_content_action'>
+                                    <a class='removeProduct' href=''><i class='fas fa-trash'></i>حذف</a>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr class="cartitem">
+                            <td class="cartitem_image"><img  src='img/p1.jpg' alt='' width='50px'/></td>
+                            <td class="cartitem_content">
+                                <h6 class="cartitem_content_name">موبایل</h6>
+                                <div class='cartitem_content_price'>
+                                    300,000
+                                    <span class="price_component">ریال</span>
+                                </div>
+                                <div class='cartitem_content_quantity'>
+                                    <div class="quantity">
+                                        <a href='' class='quantity_btn'>+</a>
+                                            <span class='quantity_value'>3</span>
+                                        <a href='' class='quantity_btn'>-</a>
+                                    </div>
+                                </div>
+                                <div class='cartitem_content_action'>
+                                    <a class='removeProduct' href=''><i class='fas fa-trash'></i>حذف</a>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr class="cartitem">
+                            <td class="cartitem_image"><img  src='img/p1.jpg' alt='' width='50px'/></td>
+                            <td class="cartitem_content">
+                                <h6 class="cartitem_content_name">موبایل</h6>
+                                <div class='cartitem_content_price'>
+                                    300,000
+                                    <span class="price_component">ریال</span>
+                                </div>
+                                <div class='cartitem_content_quantity'>
+                                    <div class="quantity">
+                                        <a href='' class='quantity_btn'>+</a>
+                                            <span class='quantity_value'>3</span>
+                                        <a href='' class='quantity_btn'>-</a>
+                                    </div>
+                                </div>
+                                <div class='cartitem_content_action'>
+                                    <a class='removeProduct' href=''><i class='fas fa-trash'></i>حذف</a>
+                                </div>
+                            </td>
+                        </tr>
+                    </table>
+                </section>
+                <section class="content-box  cart-small-content">
+                    <div class="cart-small-content-row">
+                        <div class="cart-small-content-row_key">مبلغ نهایی سفارش</div>
+                        <div class="cart-small-content-row_value">1,000,000<span class="price_component">ریال</span></div>
+                    </div>
+                    <div class="cart-small-content-row">
+                        <div class="cart-small-content-row_key">میزان تخفیف</div>
+                        <div class="cart-small-content-row_value">1,000,000<span class="price_component">ریال</span></div>
+                    </div>
+                    <button type="button" class="btn block-btn btn-primary">ادامه</button>
+                </section>
+        </section>
 </main>
